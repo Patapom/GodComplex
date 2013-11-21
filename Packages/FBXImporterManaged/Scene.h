25d3686cfb25cc4cbdd98b8e53d90a0ef209d301
Patapom <patapom@hotmail.com>
2013-11-21 21:23:12 +0100

    . Fixed FBX content loading that hasn't changed much  => Debugging consolidation and stuff...

--- Packages/FBXImporterManaged/Scene.h
+++ Packages/FBXImporterManaged/Scene.h
@@ -353,11 +353,20 @@ namespace FBXImporter
 				{	// Standard materials
 					FbxClassId	ClassID = _pMaterial->GetClassId();
 					if ( ClassID.Is( FbxSurfaceLambert::ClassId ) )
-						NewMaterial = gcnew MaterialLambert( this, dynamic_cast<FbxSurfaceLambert*>( _pMaterial ) );
+					{
+						FbxSurfaceLambert*	pMat = (FbxSurfaceLambert*) _pMaterial;
+						NewMaterial = gcnew MaterialLambert( this, pMat );
+					}
 					else if ( ClassID.Is( FbxSurfacePhong::ClassId ) )
-						NewMaterial = gcnew MaterialPhong( this, dynamic_cast<FbxSurfacePhong*>( _pMaterial ) );
+					{
+						FbxSurfacePhong*	pMat = (FbxSurfacePhong*) _pMaterial;
+						NewMaterial = gcnew MaterialPhong( this, pMat );
+					}
 					else
-						NewMaterial = gcnew Material( this, dynamic_cast<FbxSurfaceMaterial*>( _pMaterial ) );
+					{
+						FbxSurfaceMaterial*	pMat = (FbxSurfaceMaterial*) _pMaterial;
+						NewMaterial = gcnew Material( this, pMat );
+					}
 // 					else
 // 						throw gcnew Exception( "Unsupported material class ID: " + Helpers::GetString( _pMaterial->GetClassId().GetName() ) + " for material \"" + Helpers::GetString( _pMaterial->GetName() ) + "\"!" );
 				}
