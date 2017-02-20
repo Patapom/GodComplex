9976310644f3662f2adfc7b84f7e9a58299065f1
Patapom <patapom@gmail.com>
2017-02-20 18:17:56 +0100

    • Wrote the mars lander simulator to mimic the website's

--- Tests/CodinGame/Program.cs
+++ Tests/CodinGame/Program.cs
@@ -1,7 +1,8 @@
 ﻿//#define WIRE_CHALLENGE
 //#define NETWORKING_CHALLENGE
 //#define WAR_CHALLENGE
-#define DIVISORS_CHALLENGE
+//#define DIVISORS_CHALLENGE
+#define MARS_LANDER_CHALLENGE
 
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+
+using SharpMath;
 
 namespace CodinGame {
 	static class Program {
@@ -531,6 +534,112 @@ static string	ms_inputText =
 #endif
 		#endregion
 
+#region MARS LANDER CONTROLLER
+
+		static string	ms_inputText = 
+"7\r\n" + 
+"0 100\r\n" + 
+"1000 500\r\n" + 
+"1500 1500\r\n" + 
+"3000 1000\r\n" + 
+"4000 150\r\n" + 
+"5500 150\r\n" + 
+"6999 800\r\n" +
+"2500 2700 0 0 550 0 0";
+
+
+		class MarsLander {
+			class Writer : StringWriter {
+				public MarsLander	m_owner;
+				public override void WriteLine( string value ) {
+					System.Diagnostics.Debug.WriteLine( value );
+					m_owner.React( value );
+				}
+			}
+			class Reader : TextReader {
+				public List< string >	m_lines = new List< string >();
+				public override string ReadLine() {
+					string	top = m_lines[0];
+					m_lines.RemoveAt( 0 );
+					return top;
+				}
+			}
+			Writer	m_writer = new Writer();
+			Reader	m_reader = new Reader();
+
+			List< float2 >	m_landscape = new List< float2 >();
+
+			float2	Pos;
+			float2	Vel;
+			int		Fuel;
+			int		Rotation, Thrust;
+
+			public MarsLander( string _initialLines ) {
+				m_writer.m_owner = this;
+				Console.SetIn( m_reader );
+				Console.SetOut( m_writer );
+
+				string[]	initialLines = _initialLines.Split( '\n' );
+				for ( int i=0; i < initialLines.Length; i++ )
+					m_reader.m_lines.Add( initialLines[i] );
+
+				int			landscapePointsCount = int.Parse( initialLines[0] );
+				for ( int i=0; i < landscapePointsCount; i++ ) {
+					string[]	coords = initialLines[1+i].Split( ' ' );
+					m_landscape.Add( new float2( float.Parse( coords[0] ), float.Parse( coords[1] ) ) );
+				}
+
+				string[]	initialValues = initialLines[1+landscapePointsCount].Split( ' ' );
+				Pos = new float2( int.Parse( initialValues[0] ), int.Parse( initialValues[1] ) );
+				Vel = new float2( int.Parse( initialValues[2] ), int.Parse( initialValues[3] ) );
+				Fuel = int.Parse( initialValues[4] );
+				Rotation = int.Parse( initialValues[5] );
+				Thrust = int.Parse( initialValues[6] );
+			}
+
+			void	React( string _userCommands ) {
+				string[]	userCommands = _userCommands.Split( ' ' );
+				int			newRotation = int.Parse( userCommands[0] );
+				if ( newRotation < -90 || newRotation > 90 ) throw new Exception( "Too large rotation!" );
+				if ( Math.Abs( newRotation - Rotation ) > 15 ) throw new Exception( "Too large rotation delta!" );
+				int			newThrust = int.Parse( userCommands[1] );
+				if ( newThrust < 0 || newThrust > 4 ) throw new Exception( "Too large thrust!" );
+				if ( Math.Abs( newThrust - Thrust ) > 1 ) throw new Exception( "Too large thrust delta!" );
+
+				Rotation = newRotation;
+				Thrust = newThrust;
+				Fuel--;
+
+				float2	oldPos = Pos;
+
+				float2	Acc = Thrust * new float2( (float) Math.Sin( Rotation * Math.PI / 180 ), (float) Math.Cos( Rotation * Math.PI / 180 ) );
+				Vel += Acc;
+				Pos += Vel;
+				CheckCrash( oldPos );
+
+				string	newLine = ((int) Pos.x) + " " + ((int) Pos.y) + " " + ((int) Vel.x) + " " + ((int) Vel.y) + " " + Fuel + " " + Rotation + " " + Thrust;
+				m_reader.m_lines.Add( newLine );
+			}
+
+			void	CheckCrash( float2 _oldPos ) {
+				for ( int i=0; i < m_landscape.Count-1; i++ ) {
+					float2	P0 = m_landscape[i];
+					float2	P1 = m_landscape[i+1];
+					float2	N = (P1 - P0).Normalized;
+							N.Set( -N.y, N.x );
+
+					float	dot0 = (_oldPos - P0).Dot( N );
+					float	dot1 = (Pos - P0).Dot( N );
+					if ( dot0 > 0.0f && dot1 <= 0 ) {
+						throw new Exception( "Crash!" );
+					}
+				}
+			}
+
+		}
+
+#endregion
+
 		class ConsoleWriterOverride : StringWriter {
 			public override void WriteLine( string value ) {
 				System.Diagnostics.Debug.WriteLine( value );
@@ -548,6 +657,10 @@ static string	ms_inputText =
 			// Replace input and output streams
 			Console.SetIn( new StringReader( ms_inputText ) );
 			Console.SetError( new ConsoleWriterOverride() );
+
+			#if MARS_LANDER_CHALLENGE
+				MarsLander	controller = new MarsLander( ms_inputText );
+			#endif
 
 			// Execute
 			Solution.Meuh( null );
