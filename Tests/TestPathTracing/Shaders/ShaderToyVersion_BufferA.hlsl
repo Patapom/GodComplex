e45bf5a598d79d3f79d9bcc9c68b0c30f800d5e5
Patapom <patapom@gmail.com>
2017-04-24 18:02:02 +0200

    • First sequence done

--- Tests/TestPathTracing/Shaders/ShaderToyVersion_BufferA.hlsl
+++ Tests/TestPathTracing/Shaders/ShaderToyVersion_BufferA.hlsl
@@ -7,6 +7,17 @@
 #define saturate( a ) clamp( (a), 0.0, 1.0 )
 #define lerp( a, b, t ) mix( a, b, t )
 
+#define T1	12.0
+#define T2	31.5
+#define T3	60.0
+
+#define	DT0 (T1)
+#define	DT1 (T2 - T1)
+#define	DT2 (T3 - T2)
+#define	DT3 (T4 - T3)
+#define	DT4 (T5 - T4)
+#define	DT5 (T6 - T5)
+
 float rand( float n ) { return fract(sin(n) * 43758.5453123); }
 
 float rand( vec2 _seed ) {
@@ -39,42 +50,76 @@ float	BeatBump() {
     return bass;
 }
 
-vec2	Sequence( float _time ) {
+vec3	Sequence( float _time ) {
+    float	ID = 0.0;
     float	t = mod( _time, 10000.0 );
+    float	dt = 1.0;
+
+//t += T1; // Force sequence 1
+t += T2; // Force sequence 2
+    
     // 0s -> 30s = build up
-    return vec2( 0.0, t );
+    if ( t < T1 ) {
+        // 0s -> 12s => Vertical traveling
+        dt = DT0;
+    } else if ( t < T2 ) {
+        // 12s -> 30s => Bent traveling
+        t -= T1;
+        ID = 1.0;
+        dt = DT1;
+    } else if ( t < T3 ) {
+        // 30s -> XX => Flickering neons on walls
+        t -= T2;
+        ID = 2.0;
+        dt = DT2;
+    }
+    
+    return vec3( ID, t, dt );
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-// Computes sequencing for the camera & sphere
-void	ComputeSequencedElements( float _sequenceID, float t, vec2 _UV,
-                                 out vec3 _wsCameraPosition, out vec3 _wsCameraTarget,
+// Computes sequencing for the camera & sphere positions as well as wall & sphere colors
+void	ComputeSequencedElements( float _sequenceID, float t, float dt, vec2 _UV,
+                                 out vec3 _wsCameraPosition, out vec3 _wsCameraTarget, out vec3 _wsCameraUp,
                                  out vec3 _wsSphereCenter,
                                  out vec3 _colorWall,
                                  out vec3 _colorSphere ) {
-_wsCameraPosition = _wsCameraTarget = _wsSphereCenter = _colorWall = _colorSphere = vec3( 0.0 );
+	_wsCameraPosition = _wsCameraTarget = _wsSphereCenter = _colorWall = _colorSphere = vec3( 0.0 );
+    _wsCameraUp = vec3( 0.0, 1.0, 0.0 );
+
+    float	rcpDt = 1.0 / dt;
+    float	t2 = saturate( rcpDt * t );	// Time normalized to interval
     
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	//
-	if ( _sequenceID < 1.0 ) {
+	if ( _sequenceID < 2.0 ) {
         // Fixed viewpoint with slow traveling of target...
-        float	t2 = saturate( 0.1 * t );
-	    _wsCameraPosition = vec3( 0.0, -0.8, -0.5 );
-        _wsCameraTarget = vec3( 0.0, -0.8, lerp( -0.8, 0.8, t2 ) );
+//	    _wsCameraPosition = vec3( 0.0, lerp( -0.9, 0.5, t2 ), -0.2 );
+//      _wsCameraTarget = vec3( 0.0, lerp( -2.0, -0.8, t2 ), 0.8 );
 
 		// Fixed, near the corner
         _wsSphereCenter = vec3( -0.6, -0.8, 0.8 );
+
+        if ( _sequenceID < 1.0 ) {
+            // Vertical traveling
+	    	_wsCameraPosition = vec3( 0.0, lerp( -0.9, -0.2, t2 ), -0.6 );
+        	_wsCameraTarget = vec3( 0.0, lerp( -2.0, -1.2, t2*t2 ), 0.8 );
+        } else {
+            // Horizontal traveling with rolled camera
+            _wsCameraUp = normalize( vec3( 1.0, -0.6, 0.0 ) );
+	    	_wsCameraPosition = _wsSphereCenter + vec3( lerp( 0.2, 0.0, t2 ), -0.15, -0.5 );
+        	_wsCameraTarget = _wsSphereCenter + vec3( lerp( 0.2, 0.0, t2 ), -0.15, 0.0 );
+        }
         
         // Walls are dark to start with
 		_colorWall = vec3( 0.0 );
         
         // Random flashes with a vertical gradient
-//        color = vec3( BeatBump() );
-        float	buildUp = t2;//saturate( 0.1*t );
+        float	buildUp = saturate( 0.5*t );
         float	r = buildUp * noise( vec2( 3.897 * t + 17.651923, 0.0 ) );
         		r = pow( saturate( r + 1.0 * BeatBump() ), 2.0 );
-        _colorSphere = vec3( 20.0 * r * (1.0 - _UV.y) );
+        _colorSphere = vec3( 40.0 * max( 0.005, r ) * pow( 1.0 - _UV.y, 2.0 ) );
 	//
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	//
@@ -99,86 +144,9 @@ _wsCameraPosition = _wsCameraTarget = _wsSphereCenter = _colorWall = _colorSpher
 				bandColor = 0.25 + 0.75 * bandColor;
 		_colorSphere = intensity * bandColor;
     }
-}
-
-vec3	ComputeSphereCenter( vec2 _sequence ) {
-    float	t = _sequence.y;
-    vec3	pos = vec3( 0.0 );
-    if ( _sequence.x == 0.0 ) {
-        // Fixed, near the corner
-        pos = vec3( -0.6, -0.8, 0.8 );
-    } else {
-        pos = vec3( 0.6 * sin( 0.5 * t ), -0.8, 0.8 );
-    }
-	return pos;
-}
-
-void	ComputeCamera( vec2 _sequence, out vec3 _wsPos, out vec3 _wsTarget ) {
-    float	t = _sequence.y;
-    _wsPos = vec3( 0.0, -0.8, -0.5 );
-//    _wsPos = vec3( sin( t ), cos( 0.713 * t ), -0.8 );
-    _wsTarget = vec3( 0.0, -0.8, 0.0 );
-
-    if ( _sequence.x < 1.0 ) {
-        // Fixed viewpoint with slow traveling of target...
-        float	t2 = saturate( 0.1 * t );
-	    _wsPos = vec3( 0.0, -0.8, -0.5 );
-        _wsTarget = vec3( 0.0, -0.8, lerp( -0.8, 0.8, t2 ) );
-    }
-
-//_wsTarget = vec3( sin( 2.0 * t ), -0.2, 0.8 );
-}
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Computes sequencing for the walls
-//
-vec3	ComputeWallColor( vec2 _sequence, vec2 _UV ) {
-    float	t = _sequence.y;
-    vec3	color = vec3( 0.0 );
-    if ( _sequence.x == 0.0 ) {
-        // Walls are dark to start with
-		color = vec3( 0.0 );
-    } else if ( _sequence.x == 1.0 ) {
-		color = vec3( texture( iChannel0, vec2( 1.0 - _UV.x, 0.25 + 0.5 * abs( sin( t ) ) ) ).x );
-//		color = texture( iChannel1, UV ).xxx;
-		color = pow( color, vec3( 4.0 ) );
-    }
-    return color;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Computes sequencing for the sphere
-//
-vec3	ComputeSphereColor( vec2 _sequence, vec2 _UV, vec3 _wsSphereCenter ) {
-    float	t = _sequence.y;
-    vec3	color = vec3( 0.0 );
-    if ( _sequence.x == 0.0 ) {
-        // Random flashes
-//        color = vec3( BeatBump() );
-        float	buildUp = saturate( 0.1*t );
-        float	r = buildUp * noise( vec2( 3.897 * t + 17.651923, 0.0 ) );
-        		r = pow( saturate( r + 1.0 * BeatBump() ), 2.0 );
-        color = vec3( 20.0 * r * (1.0 - _UV.y) );
-        
-    } else if ( _sequence.x == 1.0 ) {
-		// Map sphere color depending on height
-//		float	V = 0.5 * (1.0 + (_wsPosition.y - _wsSphereCenter.y) * (1.0 / SPHERE_RADIUS));
-		float	V = _UV.y;
-		const float	BANDS_COUNT = 20.0;
-		V *= BANDS_COUNT;
-		float	bandIndex = floor( V );
-		float	Vband = 2.0 * fract( V ) - 1.0;
-				Vband = sqrt( 1.0 - Vband*Vband );
-		float	intensity = abs( sin( 4.0 * t + bandIndex * sin( t ) ) ) * Vband;
-		vec3	bandColor = fract( vec3( 13.289 * bandIndex, 0.9 - 3.18949 * bandIndex, 17.0 * bandIndex ) );
-				bandColor = 0.25 + 0.75 * bandColor;
-		color = intensity * bandColor;
-	}
     
-color = vec3( 10.0 );
-    
-    return color;
+//_colorSphere = vec3( BeatBump() );
+//_colorSphere = vec3( 10.0 );
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -192,42 +160,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 //    fragColor = vec4( texture( iChannel0, vec2( UV.x, 0.25 + 0.5 * abs( sin( iGlobalTime ) ) ) ).x );
 //	fragColor = pow( fragColor, vec4( 4.0 ) );
     
-	vec2	sequence = Sequence( iChannelTime[0] );
-#if 1
+	vec3	sequence = Sequence( iChannelTime[0] );
 	vec2	UV = fract( fragCoord.xy / 64.0 );
-	vec3	wsCameraPosition, wsCameraTarget, wsSphereCenter, colorWall, colorSphere;
-	ComputeSequencedElements( sequence.x, sequence.y, UV, wsCameraPosition, wsCameraTarget, wsSphereCenter, colorWall, colorSphere );
+	vec3	wsCameraPosition, wsCameraTarget, wsCameraUp, wsSphereCenter, colorWall, colorSphere;
+	ComputeSequencedElements( sequence.x, sequence.y, sequence.z ,UV, wsCameraPosition, wsCameraTarget, wsCameraUp, wsSphereCenter, colorWall, colorSphere );
 
     vec3	color = vec3( 0.0 );
     if ( fragCoord.y < 64.0 && fragCoord.x < 128.0 ) {
+        // Bottom of the texture will contain the sequenced wall & sphere textures
 		color = fragCoord.x < 64.0 ? colorWall : colorSphere;
     } else if ( fragCoord.y > iResolution.y-1.0 ) {
-        color = fragCoord.x < 1.0 ? wsCameraPosition : (fragCoord.x < 2.0 ? wsCameraTarget : wsSphereCenter);
+        // Top line will contain sequenced positions
+        color = fragCoord.x < 1.0 ? wsCameraPosition : (fragCoord.x < 2.0 ? wsCameraTarget : (fragCoord.x < 3.0 ? wsCameraUp : wsSphereCenter));
     } 
-#else    
-    vec3	sphereCenter = ComputeSphereCenter( sequence );
-    
-    vec3	color = vec3( 0.0 );
-    if ( fragCoord.y < 64.0 && fragCoord.x < 128.0 ) {
-		vec2	UV = fragCoord.xy / 64.0;
-        if ( fragCoord.x < 64.0 ) {
-            // Material 0 = Walls
-            color = ComputeWallColor( sequence, UV );
-        } else {
-            // Material 1 = Sphere
-			UV.x -= 1.0;
-            color = ComputeSphereColor( sequence, UV, sphereCenter );
-        }
-    } else if ( fragCoord.y > iResolution.y-1.0 ) {
-        // Compute camera & sphere vectors
-        if ( fragCoord.x < 2.0 ) {
-            vec3	wsCameraPos, wsCameraTarget;
-            ComputeCamera( sequence, wsCameraPos, wsCameraTarget );
-            color = fragCoord.x < 1.0 ? wsCameraPos : wsCameraTarget;
-        } else if ( fragCoord.x < 3.0 ) {
-            color = sphereCenter;
-        }
-    }
-#endif    
     fragColor = vec4( color, 1.0 );
 }
