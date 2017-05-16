5d6234aea5d366d11aa08a4e30703a494cc7fb33
Patapom <patapom@hotmail.com>
2017-05-16 18:42:49 +0200

    • Added base app for pull-push algorithm test

---
+++ Tests/TestPullPush/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TestPullPush
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panelInputImage = new TestPullPush.PanelOutput(this.components);
+			this.panelOutputReconstruction = new TestPullPush.PanelOutput(this.components);
+			this.panelPixelDensity = new TestPullPush.PanelOutput(this.components);
+			this.panelSparseInputImage = new TestPullPush.PanelOutput(this.components);
+			this.SuspendLayout();
+			// 
+			// panelInputImage
+			// 
+			this.panelInputImage.Location = new System.Drawing.Point(12, 12);
+			this.panelInputImage.Name = "panelInputImage";
+			this.panelInputImage.Size = new System.Drawing.Size(350, 350);
+			this.panelInputImage.TabIndex = 0;
+			// 
+			// panelOutputReconstruction
+			// 
+			this.panelOutputReconstruction.Location = new System.Drawing.Point(724, 12);
+			this.panelOutputReconstruction.Name = "panelOutputReconstruction";
+			this.panelOutputReconstruction.Size = new System.Drawing.Size(350, 350);
+			this.panelOutputReconstruction.TabIndex = 0;
+			// 
+			// panelPixelDensity
+			// 
+			this.panelPixelDensity.Location = new System.Drawing.Point(1080, 12);
+			this.panelPixelDensity.Name = "panelPixelDensity";
+			this.panelPixelDensity.Size = new System.Drawing.Size(350, 350);
+			this.panelPixelDensity.TabIndex = 0;
+			// 
+			// panelSparseInputImage
+			// 
+			this.panelSparseInputImage.Location = new System.Drawing.Point(368, 12);
+			this.panelSparseInputImage.Name = "panelSparseInputImage";
+			this.panelSparseInputImage.Size = new System.Drawing.Size(350, 350);
+			this.panelSparseInputImage.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1444, 377);
+			this.Controls.Add(this.panelPixelDensity);
+			this.Controls.Add(this.panelOutputReconstruction);
+			this.Controls.Add(this.panelSparseInputImage);
+			this.Controls.Add(this.panelInputImage);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Pull-Push Algorithm Test";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private PanelOutput panelInputImage;
+		private PanelOutput panelOutputReconstruction;
+		private PanelOutput panelPixelDensity;
+		private PanelOutput panelSparseInputImage;
+	}
+}
+
---
+++ Tests/TestPullPush/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SharpMath;
+using ImageUtility;
+
+namespace TestPullPush
+{
+	public partial class Form1 : Form
+	{
+		ImageFile		m_imageInput;
+		ImageFile		m_imageSparseInput;
+
+		public Form1() {
+			InitializeComponent();
+
+			m_imageInput = new ImageFile( new System.IO.FileInfo( "Parrot.png" ) );
+			panelInputImage.m_bitmap = m_imageInput.AsBitmap;
+
+			uint	W = m_imageInput.Width;
+			uint	H = m_imageInput.Height;
+
+			// Build the sparse input image which is the original image but with many holes in pixels
+			m_imageSparseInput = new ImageFile( W, H, PIXEL_FORMAT.BGRA8, m_imageInput.ColorProfile );
+			m_imageSparseInput.Clear( float4.UnitW );
+			float4[,]	inputPixels = new float4[W,H];
+			m_imageInput.ReadPixels( ( uint X, uint Y, ref float4 _color ) => { inputPixels[X,Y] = _color; } );
+			for ( uint Y=0; Y < H; Y++ ) {
+				float	fY = 2.0f * Y / H - 1.0f;
+				for ( uint X=0; X < W; X++ ) {
+					float	fX = 2.0f * X / W - 1.0f;
+					float	gauss = (float) Math.Exp( -5.0f * (fX*fX + fY*fY) );
+					float	weight = SimpleRNG.GetUniform() < gauss ? 1.0f : 0.0f;
+					inputPixels[X,Y].x *= weight;
+					inputPixels[X,Y].y *= weight;
+					inputPixels[X,Y].z *= weight;
+				}
+			}
+
+			m_imageSparseInput.WritePixels( ( uint X, uint Y, ref float4 _color ) => { _color = inputPixels[X,Y]; } );
+			panelSparseInputImage.m_bitmap = m_imageSparseInput.AsBitmap;
+		}
+
+		protected override void OnFormClosed(FormClosedEventArgs e) {
+			base.OnFormClosed(e);
+
+			m_imageInput.Dispose();
+		}
+	}
+}
