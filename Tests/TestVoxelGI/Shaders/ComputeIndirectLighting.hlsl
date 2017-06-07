23c03f0c28bc938a084267b466797792da310878
Patapom <patapom@hotmail.com>
2017-06-07 18:51:29 +0200

    • Nice concluding results for today

--- Tests/TestVoxelGI/Shaders/ComputeIndirectLighting.hlsl
+++ Tests/TestVoxelGI/Shaders/ComputeIndirectLighting.hlsl
@@ -83,14 +83,16 @@ float3	positionUVW = (wsPos - VOXEL_MIN) * INV_VOXEL_VOLUME_SIZE;	// TODO: Optim
 		// Continuous algorithm that keeps accumulating with
 		const float	ALPHA_THRESHOLD = 1.0 / 255.0;
 		const float	TRANSPARENCY_THRESHOLD = 0.1;
-		const float	EXTINCTION_FACTOR = 1.0;
+		const float	EXTINCTION_FACTOR = 10.0;
+		const float	SCATTERING_ALBEDO = 0.9;	// Scatters half as much as it extincts
 
 		float4	irradiance_alpha = _Tex_VoxelScene_SourceLighting.SampleLevel( LinearClamp, positionUVW, mipLevel );
 		if ( irradiance_alpha.w > ALPHA_THRESHOLD ) {
 			float	marchedDistance = _hitDistance - previousDistance;
 			float	extinction = exp( -EXTINCTION_FACTOR * marchedDistance * irradiance_alpha.w );	// Use alpha as a measure of density
-			irradiance.xyz += irradiance.w * irradiance_alpha.xyz;	// Add voxel's irradiance as perceived through our accumulated extinction
-			irradiance.w *= extinction;								// And accumulate extinction
+			float3	scattering = SCATTERING_ALBEDO * (1.0 - extinction) * irradiance_alpha.xyz;
+			irradiance.xyz += irradiance.w * scattering;	// Add voxel's irradiance as perceived through our accumulated extinction
+			irradiance.w *= extinction;						// And accumulate extinction
 // 			if ( irradiance.w < TRANSPARENCY_THRESHOLD )
 // 				break;	// Opaque!
 		}
@@ -139,11 +141,12 @@ void	CS( uint3 _GroupID : SV_GROUPID, uint3 _GroupThreadID : SV_GROUPTHREADID, u
 	float3	T, B;
 	BuildOrthonormalBasis( N, T, B );
 	
+#if 0	// DEBUG
 float	hitDistance;
 float3	wsConeDirection = N;
 float4	irradiance = ConeTrace2( wsVoxelCenter, wsConeDirection, VOXEL_SIZE / max( max( abs( wsConeDirection.x ), abs( wsConeDirection.y ) ), abs( wsConeDirection.z ) ), 0.5, hitDistance );
 //float3	value = 0.05 * hitDistance;
-float3	value = 1.0 * irradiance.xyz;
+float3	value = 2.0 * PI * irradiance.xyz;
 //float3	value = irradiance.w;
 //float3	value = N;
 //float3	wsPos = wsVoxelCenter + 0.5 * VOXEL_SIZE * N;
@@ -153,31 +156,35 @@ float3	value = 1.0 * irradiance.xyz;
 //float3	value = pow2( temp.xyz );
 _Tex_VoxelScene_TargetLighting[voxelIndex] = float4( value, 0 );
 
+#else
+	// Cast 7 cones to cover the top hemisphere
+	const uint		CONES_COUNT = 7;
+	const float		CONE_APERTURE = 60.0 * PI / 180.0;		// 60° aperture
+	const float		SOLID_ANGLE = 2.0 * PI / CONES_COUNT;	// The sum of all cones must cover the 2PI steradians hemisphere
+	const float		SIN_HALF_ANGLE = 0.5;						// sin( 30 )
+	const float3	LS_CONE_DIRECTIONS[7] = {	float3( 0, 0, 1 ),
+												float3( 0.8660254, 0, 0.5 ),
+												float3( 0.4330127, 0.7500001, 0.5 ),
+												float3( -0.4330128, 0.75, 0.5 ),
+												float3( -0.8660254, -7.571035E-08, 0.5 ),
+												float3( -0.4330126, -0.7500001, 0.5 ),
+												float3( 0.433013, -0.7499999, 0.5 ),
+											};
 
-// 	// Cast 7 cones to cover the top hemisphere
-// 	const uint		CONES_COUNT = 7;
-// 	const float		CONE_APERTURE = 60.0 * PI / 180.0;		// 60° aperture
-// 	const float		SOLID_ANGLE = 2.0 * PI / CONES_COUNT;	// The sum of all cones must cover the 2PI steradians hemisphere
-// 	const float		SIN_HALF_ANGLE = 0.5;						// sin( 30 )
-// 	const float3	LS_CONE_DIRECTIONS[7] = {	float3( 0, 0, 1 ),
-// 												float3( 0.8660254, 0, 0.5 ),
-// 												float3( 0.4330127, 0.7500001, 0.5 ),
-// 												float3( -0.4330128, 0.75, 0.5 ),
-// 												float3( -0.8660254, -7.571035E-08, 0.5 ),
-// 												float3( -0.4330126, -0.7500001, 0.5 ),
-// 												float3( 0.433013, -0.7499999, 0.5 ),
-// 											};
-// 
-// 	float3	sumIrradiance = 0.0;
-// 	[unroll]
-// 	for ( uint coneIndex=0; coneIndex < CONES_COUNT; coneIndex++ ) {
-// 		float3	lsConeDirection = LS_CONE_DIRECTIONS[coneIndex];
-// 		float3	wsConeDirection = lsConeDirection.x * T + lsConeDirection.y * B + lsConeDirection.z * N;
-// 		float	initialDistance = 0.5 * VOXEL_SIZE / lsConeDirection.z;	// Offset half a voxel away
-// 		float	hitDistance;
-// 		float3	irradiance = ConeTrace( wsVoxelCenter, wsConeDirection, initialDistance, SIN_HALF_ANGLE, hitDistance );
-// 		sumIrradiance += irradiance * lsConeDirection.z * SOLID_ANGLE;
-// 	}
-// 
-// 	_Tex_VoxelScene_TargetLighting[voxelIndex] = float4( sumIrradiance * INVPI * albedo.xyz, 0.0 );
+	float3	sumIrradiance = 0.0;
+	[unroll]
+	for ( uint coneIndex=0; coneIndex < CONES_COUNT; coneIndex++ ) {
+		float3	lsConeDirection = LS_CONE_DIRECTIONS[coneIndex];
+		float3	wsConeDirection = lsConeDirection.x * T + lsConeDirection.y * B + lsConeDirection.z * N;
+//		float	initialDistance = 0.5 * VOXEL_SIZE / lsConeDirection.z;	// Offset half a voxel away
+		float	initialDistance = VOXEL_SIZE / max( max( abs( wsConeDirection.x ), abs( wsConeDirection.y ) ), abs( wsConeDirection.z ) );
+		float	hitDistance;
+//		float3	irradiance = ConeTrace( wsVoxelCenter, wsConeDirection, initialDistance, SIN_HALF_ANGLE, hitDistance );
+		float4	irradiance = ConeTrace2( wsVoxelCenter, wsConeDirection, initialDistance, SIN_HALF_ANGLE, hitDistance );
+		sumIrradiance += irradiance.xyz * lsConeDirection.z;
+	}
+	sumIrradiance *= 2.0 * PI;
+
+	_Tex_VoxelScene_TargetLighting[voxelIndex] = float4( sumIrradiance * INVPI * albedo.xyz, 0.0 );
+#endif
 }
