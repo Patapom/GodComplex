19f8bf20705a00442d7fa5d618eedc8fd2c699a1
Patapom <patapom@hotmail.com>
2018-01-23 21:11:58 +0100

    • Reproduced bent cone computation  => Need to check average cone angle + variance

--- Tests/TestGroundTruthAOFitting/Shaders/Demo/ComputeIndirectIrradiance2.hlsl
+++ Tests/TestGroundTruthAOFitting/Shaders/Demo/ComputeIndirectIrradiance2.hlsl
@@ -11,6 +11,9 @@
 
 #define	SAMPLER	LinearWrap
 
+#define PREMULTIPLY_ALBEDO 1	// Define this to store (albedo/PI)*Irradiance instead of simply Irradiance
+								// The main interest is to be able to use the value straight from the sampler
+								//	next frame and avoid to sample the albedo buffer to redo the computation manually...
 
 cbuffer	CBCompute : register( b2 ) {
 	uint2	_textureDimensions;	// Height map resolution
@@ -36,41 +39,38 @@ struct PS_OUT {
 };
 
 VS_IN	VS( VS_IN _In ) { return _In; }
-/*
-float2	CameraSpacePosition_mm2UV( float3 _position_mm ) {
-	return _position_mm.xy / (_texelSize_mm * _textureDimensions);
-}
-float3	GetCameraSpacePosition_mm( float2 _pixelPosition ) {
-	float3	position_mm = float3( _texelSize_mm * _pixelPosition, 0.0 );
-//			position_mm.z = _displacement_mm * _texHeight.SampleLevel( SAMPLER, CameraSpacePosition_mm2UV( position_mm ), 0.0 );
-			position_mm.z = _displacement_mm * _texHeight.SampleLevel( SAMPLER, _pixelPosition / _textureDimensions, 0.0 );
-	return position_mm;
-}
-// Projects a camera space position into surface's local tangent space
-float3	Project( float3 _csPosition_mm, float3 _csCenterPosition_mm, float3 _T, float3 _B, float3 _N ) {
-	float3	csDeltaPosition_mm = _csPosition_mm - _csCenterPosition_mm;
 
-	float	verticalRatio = _texelSize_mm / _displacement_mm;
-	_T.z *= verticalRatio;
-	_B.z *= verticalRatio;
-	_N.z *= verticalRatio;
-
-	return float3( dot( csDeltaPosition_mm, _T ), dot( csDeltaPosition_mm, _B ), dot( csDeltaPosition_mm, _N ) );
-}
-
-// Computes the integration of the spherical solid angle between two elevations given by their tangents
-// We are given the integral:
-//		Lo(x,wo) = Integral{0,PI/2}( Li(x,wi) (wi.n) dwi )
-// If we assume Li(x,wi) is constant then we are left with the simple integral:
-//		Lo(x,wo) = Li(x,wi) Integral{0,PI/2}( (wi.n) dwi )
-// dwi is the PHI part of the integrand that depends on theta: sin(theta) dtheta and finally
-//		Lo(x,wo) = Li(x,wi) Integral{0,PI/2}( cos(theta) sin(theta) dtheta ) = Li(x,wi) * [-1/2 cos²(theta)]{0,PI/2} = Li(x,wi) * 0.5 * [cos²(theta0) - cos²(theta1)]
-// We let theta0 and theta1 be free here since we don't necessarily need to integrate over the entire hemisphere
+// Integrates the dot product of the normal with a vector interpolated between slice horizon angle theta0 and theta1
+// We're looking to obtain the irradiance reflected from a tiny slice of terrain:
+//	E = Integral[theta0, theta1]{ (rho(Xi)/PI * E(Xi)) (Wi.N) sin(theta) dTheta }
+// Where
+//	Xi, the location of the neighbor surface reflecting radiance
+//	rho(Xi), the surface's albedo at that location
+//	E(Xi), the surface's irradiance at that location
+//	Wi, the direction of the vector within the slice that we're integrating Wi = { cos(phi).sin(theta), sin(phi).sin(theta), cos(theta) } expressed in screen space
+//	phi, the azimutal angle of the screen-space slice
 //
-float	IntegrateSolidAngle( float _cos2Theta0, float _cos2Theta1 ) {
-	return 0.5 * (_cos2Theta0 - _cos2Theta1);
+// The integral transforms into an irradiance-agnostic version:
+//	E = (rho(Xi)/PI * E(Xi)) * Integral[theta0, theta1]{ (Wi.N) sin(theta) dTheta }
+//
+//	I = Integral[theta0, theta1]{ (Wi.N) sin(theta) dTheta }
+//	  = Integral[theta0, theta1]{ cos(phi).sin(theta).Nx.sin(theta) dTheta }	<= I0
+//	  + Integral[theta0, theta1]{ sin(phi).sin(theta).Ny.sin(theta) dTheta }	<= I1
+//	  + Integral[theta0, theta1]{          cos(theta).Nz.sin(theta) dTheta }	<= I2
+//
+//	I0 = [cos(phi).Nx] * Integral[theta0, theta1]{ sin²(theta) dTheta } = [cos(phi).Nx] * { 1/2 * (theta - sin(theta)*cos(theta)) }[theta0,theta1]
+//	I1 = [sin(phi).Ny] * Integral[theta0, theta1]{ sin²(theta) dTheta } = [sin(phi).Ny] * { 1/2 * (theta - sin(theta)*cos(theta)) }[theta0,theta1]
+//	I2 = Nz * Integral[theta0, theta1]{ cos(theta).sin(theta) dTheta } = Nz * { 1/2 * cos²(theta)) }[theta1,theta0]	<== Watch out for theta reverse here!
+//
+float	IntegrateSolidAngle( float2 _sinCosPhi, float _cosTheta0, float _cosTheta1, float3 _N ) {
+	float	theta0 = acos( _cosTheta0 );
+	float	theta1 = acos( _cosTheta1 );
+	float	I0 = (theta1 - sqrt( 1.0 - _cosTheta1*_cosTheta1 )*_cosTheta1)
+			   - (theta0 - sqrt( 1.0 - _cosTheta0*_cosTheta0 )*_cosTheta0);
+	float	I1 = _cosTheta1*_cosTheta1 - _cosTheta0*_cosTheta0;
+	return 0.5 * ((_sinCosPhi.y * _N.x + _sinCosPhi.x * _N.y) * I0 + _N.z * I1);
 }
-*/
+
 // Samples the radiance reflected from a screen-space position located around the center pixel position
 //	_ssPosition, the screen-space position to sample
 //	_radius, the radius from the center position
@@ -78,25 +78,30 @@ float	IntegrateSolidAngle( float _cos2Theta0, float _cos2Theta1 ) {
 //	_centerRho, the reflectance of the center pixel (fallback only necessary if no albedo map is available)
 //	_maxCos, the floating max cos(theta) that indicates the perceived horzion
 //
-float3	SampleRadiance( float2 _ssPosition, float _radius, float3 _T, float3 _B, float3 _N, float3 _centerRho, inout float _maxCos ) {
+float3	SampleRadiance( float2 _ssPosition, float _H0, float _radius, float2 _sinCosPhi, float3 _T, float3 _B, float3 _N, float3 _centerRho, inout float _maxCos ) {
 
 	// Sample new height and update horizon angle
-	float	H = (_displacement_mm / _texelSize_mm) * _texHeight.SampleLevel( SAMPLER, _ssPosition / _textureDimensions, 0.0 );
-	float	H2 * H * H;
-	float	hyp2 = _radius*_radius + H2;	// Square hypotenuse
-	float	cosHorizon = H / sqrt( hyp2 );	// Cosine to horizon angle
+	float	deltaH = (_displacement_mm / _texelSize_mm) * _texHeight.SampleLevel( SAMPLER, _ssPosition / _textureDimensions, 0.0 ) - _H0;
+	float	H2 = deltaH * deltaH;
+	float	hyp2 = _radius*_radius + H2;		// Square hypotenuse
+	float	cosHorizon = deltaH / sqrt( hyp2 );	// Cosine to horizon angle
 	if ( cosHorizon <= _maxCos )
 		return 0.0;	// Below the horizon... No visible contribution.
 
 	// Compute bounced incoming radiance
-	float2	neighborUV = CameraSpacePosition_mm2UV( csNeighborCameraPosition_mm );
-	float3	neighborIrradiance = _texSourceIrradiance.SampleLevel( SAMPLER, neighborUV, 0.0 ).xyz;
-//	float3	neighborRho = _texSourceAlbedo.SampleLevel( SAMPLER, neighborUV, 0.0 ).xyz;		// #TODO: Sample albedo G-buffer entry for better reflectance
-	float3	neighborRho = _centerRho;														// Uniform reflectance in neighborhood
+	float2	neighborUV = _ssPosition / _textureDimensions;
 
-	float3	incomingRadiance = (neighborRho / PI) * neighborIrradiance;						// Li-1
-//			incomingRadiance *= IntegrateSolidAngle( _maxCos2, cos2 );						// * Integral{theta0, theta1}( cos(theta) sin(theta) dtheta )
-			incomingRadiance *= 0.5 * (cos2 - _maxCos2);									// * Integral{theta0, theta1}( cos(theta) sin(theta) dtheta ) = [cos²(theta)/2]{theta0, theta1}
+	#if PREMULTIPLY_ALBEDO
+		float3	incomingRadiance = _texSourceIrradiance.SampleLevel( SAMPLER, neighborUV, 0.0 ).xyz;	// Directly Li-1 (Ei is already pre-multiplied by albedo/rho from last frame so don't bother!)
+	#else
+		float3	neighborIrradiance = _texSourceIrradiance.SampleLevel( SAMPLER, neighborUV, 0.0 ).xyz;	// Only Ei is available, Li need to be computed by fetching albedo
+//		float3	neighborRho = _texSourceAlbedo.SampleLevel( SAMPLER, neighborUV, 0.0 ).xyz;				// #TODO: Sample albedo G-buffer entry for better reflectance
+		float3	neighborRho = _centerRho;																// Uniform reflectance in neighborhood
+		float3	incomingRadiance = (neighborRho / PI) * neighborIrradiance;								// Li-1
+	#endif
+
+	// Integrate over horizon difference
+	incomingRadiance *= IntegrateSolidAngle( _sinCosPhi, _maxCos, cosHorizon, _N );
 
 	// #TODO: Integrate with linear interpolation of irradiance as well??
 
@@ -107,10 +112,13 @@ float3	SampleRadiance( float2 _ssPosition, float _radius, float3 _T, float3 _B, 
 
 PS_OUT	PS( VS_IN _In ) {
 	// Retrieve central height and TBN
+	float	H0 = (_displacement_mm / _texelSize_mm) * _texHeight.SampleLevel( SAMPLER, _In.__Position.xy / _textureDimensions, 0.0 );
 	float3	N = 2.0 * _texNormal[_In.__Position.xy].xyz - 1.0;
-//			N.y *= -1.0;
+			N.y *= -1.0;
 	float3	T, B;
 	BuildOrthonormalBasis( N, T, B );
+
+	float	recZdotN = abs(N.z) > 1e-6 ? 1.0 / N.z : 1e6 * sign(N.z);
 
 	float3	centerRho = _rho;	// Uniform reflectance
 
@@ -133,24 +141,24 @@ PS_OUT	PS( VS_IN _In ) {
 		sincos( phi, ssDirection.y, ssDirection.x );
 
 		// Project screen-space direction onto tangent plane to determine max possible horizon angles
-		float	recZdotN = abs(N.z) > 1e-6 ? 1.0 / N.z : 1e6 * sign(N.z);
 		float	hitDistance_Front = -dot( ssDirection, N.xy ) * recZdotN;
 		float3	tsDirection_Front = normalize( float3( ssDirection, hitDistance_Front ) );
 		float	maxCos_Front = tsDirection_Front.z;
 		float	maxCos_Back = -tsDirection_Front.z;
 
-		// Accumulate perceived irradiance in front and back & update floating horizon (in the form of cos(horizon angle))
+//*		// Accumulate perceived irradiance in front and back & update floating horizon (in the form of cos(horizon angle))
 		float2	ssPosition_Front = _In.__Position.xy;
 		float2	ssPosition_Back = _In.__Position.xy;
 		float2	ssStep = ssDirection * RADIUS_STEP_SIZE;
-
+		float	radius = 0.0;
 		for ( uint radiusIndex=1; radiusIndex <= MAX_RADIUS; radiusIndex++ ) {
 			ssPosition_Front += ssStep;
 			ssPosition_Back -= ssStep;
-			sumIrradiance += SampleRadiance( ssPosition_Front, radius, T, B, N, centerRho, maxCos_Front );	// Sample forward
-			sumIrradiance += SampleRadiance( ssPosition_Back, radius, T, B, N, centerRho, maxCos_Back );	// Sample backward
+			radius += RADIUS_STEP_SIZE;
+			sumIrradiance += SampleRadiance( ssPosition_Front, H0, radius, ssDirection.yx, T, B, N, centerRho, maxCos_Front );	// Sample forward
+			sumIrradiance += SampleRadiance( ssPosition_Back, H0, radius, ssDirection.yx, T, B, N, centerRho, maxCos_Back );	// Sample backward
 		}
-
+//*/
 		// Accumulate bent normal direction by rebuilding and averaging the front & back horizon vectors
 		// Half brute force where we perform the integration numerically as a sum...
 		// This solution is prefered to the analytical integral that shows some precision artefacts unfortunately...
@@ -162,18 +170,19 @@ PS_OUT	PS( VS_IN _In ) {
 		const uint	STEPS_COUNT = 256;
 		for ( uint i=0; i < STEPS_COUNT; i++ ) {
 			float	theta = lerp( thetaBack, thetaFront, (i+0.5) / STEPS_COUNT );
-			float2	scTheta;
-			sincos( theta, scTheta.x, scTheta.y );
-			float3	ssUnOccludedDirection = float3( scTheta.x * ssDirection, scTheta.y );
+			float	sinTheta, cosTheta;
+			sincos( theta, sinTheta, cosTheta );
+			float3	ssUnOccludedDirection = float3( sinTheta * ssDirection, cosTheta );
 
 			float	cosAlpha = saturate( dot( ssUnOccludedDirection, N ) );
 
-			float	weight = cosAlpha * abs(scTheta.x);		// cos(alpha) * sin(theta).dTheta  (be very careful to take abs(sin(theta)) because our theta crosses the pole and becomes negative here!)
+			float	weight = cosAlpha * abs(sinTheta);		// cos(alpha) * sin(theta).dTheta  (be very careful to take abs(sin(theta)) because our theta crosses the pole and becomes negative here!)
 			ssBentNormal += weight * ssUnOccludedDirection;
 		}
 
 		float	dTheta = (thetaFront - thetaBack) / STEPS_COUNT;
 		ssBentNormal *= dTheta;
+
 		ssBentNormal = normalize( ssBentNormal );
 		ssAverageBentNormal += ssBentNormal;
 
@@ -210,15 +219,25 @@ PS_OUT	PS( VS_IN _In ) {
 
 //sumIrradiance =0;
 
+
+	#if PREMULTIPLY_ALBEDO
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// This MUST be done either now, or later when the irradiance is used!
+	// My advice is to use it right now before writing to the texture so next frame, (rho/PI) * Irradiance is available straight from the sampler
+	//	and this avoids to sample the neighbor albedo map
+		sumIrradiance *= (centerRho / PI);
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	#endif
+
+
 	PS_OUT	Out;
 	Out.irradiance = float4( sumIrradiance, 0.0 );
-	Out.bentCone = float4( max( 0.01, cos( averageConeAngle ) ) * ssAverageBentNormal, 1.0 - stdDeviation );
+//	Out.bentCone = float4( max( 0.01, cos( averageConeAngle ) ) * ssAverageBentNormal, 1.0 - stdDeviation );
+ssAverageBentNormal.y *= -1.0;
+Out.bentCone = float4( ssAverageBentNormal, 1.0 );
 
-//Out.bentCone.xyz = csCenterPosition_mm / 1024.0;
-//Out.bentCone.xyz = N;
-//Out.bentCone.xyz = 0.5 * (1.0+N);
-//Out.bentCone.xyz = 0.5 * (1.0 + csBentNormal);
-//Out.bentCone = _texelSize_mm / (1000.0 / 512) / 2;
+//ssAverageBentNormal.y *= -1.0;
+//Out.bentCone = float4( 0.5 * (1.0+ssAverageBentNormal), 1.0 );
 
 	return Out;
 }
