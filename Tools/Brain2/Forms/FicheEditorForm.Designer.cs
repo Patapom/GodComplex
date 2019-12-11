f78aa05586fda5f429f5cefe823c907b53c26a03
Patapom <patapom@hotmail.com>
2019-12-11 10:51:38 -0500

    • Finally managed to drop WebEditor control into my fiche editor!

--- Tools/Brain2/Forms/FicheEditorForm.Designer.cs
+++ Tools/Brain2/Forms/FicheEditorForm.Designer.cs
@@ -27,6 +27,9 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.richTextBoxTitle = new System.Windows.Forms.RichTextBox();
 			this.label2 = new System.Windows.Forms.Label();
+			this.webEditor = new ZetaHtmlEditControl.Pom.WebEditor();
+			this.label3 = new System.Windows.Forms.Label();
+			this.richTextBoxTags = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
 			// folderBrowserDialog
@@ -45,9 +48,10 @@
 			// 
 			// richTextBoxTitle
 			// 
-			this.richTextBoxTitle.Location = new System.Drawing.Point(61, 32);
+			this.richTextBoxTitle.Location = new System.Drawing.Point(52, 32);
+			this.richTextBoxTitle.Multiline = false;
 			this.richTextBoxTitle.Name = "richTextBoxTitle";
-			this.richTextBoxTitle.Size = new System.Drawing.Size(623, 26);
+			this.richTextBoxTitle.Size = new System.Drawing.Size(632, 26);
 			this.richTextBoxTitle.TabIndex = 1;
 			this.richTextBoxTitle.Text = "";
 			// 
@@ -55,11 +59,40 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-			this.label2.Location = new System.Drawing.Point(12, 38);
+			this.label2.Location = new System.Drawing.Point(3, 36);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(43, 20);
 			this.label2.TabIndex = 0;
 			this.label2.Text = "Title";
+			// 
+			// webEditor
+			// 
+			this.webEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.webEditor.Location = new System.Drawing.Point(5, 96);
+			this.webEditor.Name = "webEditor";
+			this.webEditor.Size = new System.Drawing.Size(686, 403);
+			this.webEditor.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+			this.label3.Location = new System.Drawing.Point(3, 68);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(48, 20);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Tags";
+			// 
+			// richTextBoxTags
+			// 
+			this.richTextBoxTags.Location = new System.Drawing.Point(52, 64);
+			this.richTextBoxTags.Multiline = false;
+			this.richTextBoxTags.Name = "richTextBoxTags";
+			this.richTextBoxTags.Size = new System.Drawing.Size(632, 26);
+			this.richTextBoxTags.TabIndex = 1;
+			this.richTextBoxTags.Text = "";
 			// 
 			// FicheEditorForm
 			// 
@@ -67,6 +100,9 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ControlDark;
 			this.ClientSize = new System.Drawing.Size(696, 504);
+			this.Controls.Add(this.webEditor);
+			this.Controls.Add(this.richTextBoxTags);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.richTextBoxTitle);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
@@ -88,5 +124,8 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.RichTextBox richTextBoxTitle;
 		private System.Windows.Forms.Label label2;
+		private ZetaHtmlEditControl.Pom.WebEditor webEditor;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.RichTextBox richTextBoxTags;
 	}
 }
