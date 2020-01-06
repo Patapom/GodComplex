ad8ff270c5e81598e3e341c51714853a2c57146f
Patapom <patapom@hotmail.com>
2020-01-05 19:07:07 -0500

    • Web page is almost completely read but the async image rendering is never returning for some reason! :'(

--- Packages/HTMLPageRenderer/HTMLPageControl.cs
+++ Packages/HTMLPageRenderer/HTMLPageControl.cs
@@ -12,6 +12,8 @@ namespace HTMLPageRenderer {
 
 // TODO:
 //	• Use schemes to handle local files: https://github.com/cefsharp/CefSharp/wiki/General-Usage#scheme-handler
+//	• Example: Capture Full Page Using Scrolling https://github.com/WildGenie/OSIRTv2/blob/3e60d3ce908a1d25a7b4633dc9afdd53256cbb4f/OSIRT/Browser/MainBrowser.cs#L300
+//	• Download element rectangles! => Use JS
 
 	/// <summary>
 	/// Class wrapping CEF Sharp (Chromium Embedded Framework, .Net wrapper version) to render web pages in an offscreen bitmap
@@ -54,7 +56,7 @@ namespace HTMLPageRenderer {
 		private WebPageRendered			m_pageRendered;
 		private WebPageErrorOccurred	m_pageError;
 
-		public HTMLPageControl( string _url, int _pageWidth, WebPageRendered _pageRendered, WebPageErrorOccurred _pageError ) {
+		public HTMLPageControl( string _url, int _browserViewportWidth, int _maxPageHeight, WebPageRendered _pageRendered, WebPageErrorOccurred _pageError ) {
 			m_URL = _url;
 			m_pageRendered = _pageRendered;
 			m_pageError = _pageError;
@@ -86,26 +88,42 @@ namespace HTMLPageRenderer {
 
 			m_timer.Tick += timer_Tick;
 
-			m_browser.Size = new System.Drawing.Size( _pageWidth, _pageWidth * 9 / 16 );
+			if ( _maxPageHeight == 0 )
+				_maxPageHeight = _browserViewportWidth * 9 / 16;
+
+			m_browser.Size = new System.Drawing.Size( _browserViewportWidth, _maxPageHeight );
 
 			m_browser.BrowserInitialized += browser_BrowserInitialized;
 		}
 
 		private void browser_BrowserInitialized(object sender, EventArgs e) {
+System.Diagnostics.Debug.WriteLine( "browser_BrowserInitialized" );
+
 			m_browser.Load( m_URL );
 		}
 
 		private void browser_LoadError(object sender, LoadErrorEventArgs e) {
+System.Diagnostics.Debug.WriteLine( "browser_LoadError: " + e.ErrorText );
+
+			switch ( (uint) e.ErrorCode ) {
+				case 0xffffffe5U:
+					return;	// Ignore...
+			}
+
 			m_pageError( (int) e.ErrorCode, e.ErrorText );
 		}
 
 		private void browser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e) {
+System.Diagnostics.Debug.WriteLine( "browser_FrameLoadEnd" );
+
 			RestartTimer();
 		}
 
 		private void browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e) {
 			if ( e.IsLoading )
 				return;
+
+System.Diagnostics.Debug.WriteLine( "browser_LoadingStateChanged" );
 
 			RestartTimer();
 		}
@@ -115,23 +133,43 @@ namespace HTMLPageRenderer {
 			m_timer.Enabled = true;
 		}
 
+		bool	m_contentQueried = false;
 		private void timer_Tick(object sender, EventArgs e) {
 			m_timer.Enabled = false;	// Prevent any further tick
 
-			try {
-				// Read back HTML content and do a screenshot
-				HTMLSourceReader	sourceReader = new HTMLSourceReader();
-				m_browser.GetBrowser().MainFrame.GetSource( sourceReader );
+			if ( m_contentQueried )
+				return;
 
-source null et bitmap null! Flûte caca boudin!
+			m_contentQueried = true;	// Don't re-enter!
+			QueryContent();
+		}
+
+		async Task<int>	QueryContent() {
+			try {
+System.Diagnostics.Debug.WriteLine( "QueryContent for " + m_URL );
+
+				// Read back HTML content and do a screenshot
+// 				HTMLSourceReader	sourceReader = new HTMLSourceReader();
+// 				m_browser.GetBrowser().MainFrame.GetSource( sourceReader );
+
+				// From Line 162 https://github.com/WildGenie/OSIRTv2/blob/3e60d3ce908a1d25a7b4633dc9afdd53256cbb4f/OSIRT/Browser/MainBrowser.cs#L300
+				string	source = await m_browser.GetBrowser().MainFrame.GetSourceAsync();
+				if ( source == null )
+					throw new Exception( "Failed to retrieve HTML source!" );
+
+System.Diagnostics.Debug.WriteLine( "QueryContent() => Retrieved HTML code " + (source.Length < 100 ? source : source.Remove( 100 )) );
 
 				ImageUtility.ImageFile	image = null;
-				using ( System.Drawing.Bitmap B = m_browser.ScreenshotOrNull( PopupBlending.Main ) ) {
+//				using ( System.Drawing.Bitmap B = m_browser.ScreenshotOrNull( PopupBlending.Main ) ) {
+				using ( System.Drawing.Bitmap B = await m_browser.ScreenshotAsync( true, PopupBlending.Main ) ) {
 					image = new ImageUtility.ImageFile( B, new ImageUtility.ColorProfile( ImageUtility.ColorProfile.STANDARD_PROFILE.sRGB ) );
 				}
 
+System.Diagnostics.Debug.WriteLine( "QueryContent() => Retrieved web page image" );
+
 				try {
-					m_pageRendered( sourceReader.m_HTMLContent, image );
+//					m_pageRendered( sourceReader.m_HTMLContent, image );
+					m_pageRendered( source, image );
 				} catch ( Exception _e ) {
 					throw new Exception( "An error occurred during the notification of web page successfully loaded!", _e );
 				} finally {
@@ -139,8 +177,10 @@ source null et bitmap null! Flûte caca boudin!
 				}
 
 			} catch ( Exception _e ) {
-				throw new Exception( "An error occurred while attempting to retrieve HTML source and page screenshot!", _e );
+				m_pageError( -1, "An error occurred while attempting to retrieve HTML source and page screenshot!\r\n" + _e.Message );
 			}
+
+			return 0;
 		}
 
 		class HTMLSourceReader : CefSharp.IStringVisitor {
