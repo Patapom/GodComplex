796e3a81c38a26e60e6b69fba28888b952fb3c34
Patapom <patapom@hotmail.com>
2020-10-31 10:22:39 -0400

    • Fixed "\line " missing a trailing space to properly be accounted for in RTF log text box

--- Packages/UIUtility/Forms/LogTextBox.cs
+++ Packages/UIUtility/Forms/LogTextBox.cs
@@ -24,48 +24,43 @@ namespace UIUtility
 			InitializeComponent();
 		}
 
-		public void		Log( string _Text )
-		{
-			_Text = _Text.Replace( "\n", @"\line" );
+		void	FixText( ref string _Text ) {
+			_Text = _Text.Replace( "\\", @"\" );
+			_Text = _Text.Replace( "\n", @"\line " );
 			_Text = _Text.Replace( "\r", "" );
+		}
+
+		public void		Log( string _Text ) {
+			FixText( ref _Text );
 			m_Log.Append( @"\cf1 " + _Text );
 			UpdateRTF();
 		}
 
-		public void		LogSuccess( string _Text )
-		{
-			_Text = _Text.Replace( "\n", @"\line" );
-			_Text = _Text.Replace( "\r", "" );
+		public void		LogSuccess( string _Text ) {
+			FixText( ref _Text );
 			m_Log.Append( @"\cf2 " + _Text );
 			UpdateRTF();
 		}
 
-		public void		LogWarning( string _Text )
-		{
-			_Text = _Text.Replace( "\n", @"\line" );
-			_Text = _Text.Replace( "\r", "" );
+		public void		LogWarning( string _Text ) {
+			FixText( ref _Text );
 			m_Log.Append( @"\cf3 " + _Text );
 			UpdateRTF();
 		}
 
-		public void		LogError( string _Text )
-		{
-			_Text = _Text.Replace( "\n", @"\line" );
-			_Text = _Text.Replace( "\r", "" );
+		public void		LogError( string _Text ) {
+			FixText( ref _Text );
 			m_Log.Append( @"\cf4 " + _Text );
 			UpdateRTF();
 		}
 
-		public void		LogDebug( string _Text )
-		{
-			_Text = _Text.Replace( "\n", @"\line" );
-			_Text = _Text.Replace( "\r", "" );
+		public void		LogDebug( string _Text ) {
+			FixText( ref _Text );
 			m_Log.Append( @"\cf5 " + _Text );
 			UpdateRTF();
 		}
 
-		protected void	UpdateRTF()
-		{
+		protected void	UpdateRTF() {
 			string RTFText = @"{\rtf1\ansi\deff0" +
 			@"{\colortbl;\red0\green0\blue0;\red32\green128\blue32;\red192\green128\blue0;\red255\green0\blue0;\red127\green20\blue127;}" +
 			m_Log.ToString() + "}";
